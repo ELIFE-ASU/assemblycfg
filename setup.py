d5b83425f62a1c8cfe7f265f94e4510d075912d9
Louie Slocombe <louies@hotmail.co.uk>
2024-10-15 20:30:07 -0700

    started adding project files

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CFG',
+    version='0.0.1',
+    author='',
+    author_email='',
+    description='AI calculater using smallest grammar algorithm re-pair.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ELIFE-ASU/CFG',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+    install_requires=[
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-cov',
+        ],
+    },
+)
